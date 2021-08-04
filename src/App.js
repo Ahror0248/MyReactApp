819ca9465788f4200a9aca02daf60a8a489e065c
Ahror0248 <akhrorcodrr@gmail.com>
2021-08-04 14:09:33 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div class="header-container">
+          <a href="www.fdsfs.com"><img alt="rasm bu" class="osten" src="img/osten.svg"/></a>
+          <ul>
+            <li>
+              <a href="www.gggg.com"> The Home</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> About us</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> production</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> services</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> products</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> Constructors</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> news</a>
+            </li>
+            <li>
+              <a href="www.gggg.com"> contacts</a>
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
   );
